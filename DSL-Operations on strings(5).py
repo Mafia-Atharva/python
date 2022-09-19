254e196309765f337593044f1f98aa3f1200db3a
Mafia_Atharva <97020226+Mafia-Atharva@users.noreply.github.com>
2022-09-19 03:34:42 -0700

    Update DSL-Operations on strings(5).py

--- DSL-Operations on strings(5).py
+++ DSL-Operations on strings(5).py
@@ -34,47 +34,22 @@ def longest():
             index=i
     print("The longest string is",main[index],"\n")
 
-#Function to check longest string
-def length():
-    n=int(input("Enter number of strings:"))
-    main=[]
-    for i in range(n):
-        word=input("Enter string "+str(i+1)+":")
-        main.append(word)
-    
-    def len(main):
-        cnt=0
-        for i in main:
-            cnt=cnt+1
-        return cnt
-    
-    submain=[]
-    for i in range(n):
-        x=len(main[i])
-        submain.append(x)
-    
-    max=submain[0]
-    for i in range(n):
-        if(submain[i]>max):
-            max=submain[i]
-    print("The length of longest string is",max,"\n")
-
 #Function to frequency
 def frequency():
-    string=input("Enter string to check frequency of occurence:")
-    char=input("Enter the alphabet to check:")
+    string=input("Enter string:")
+    char=input("Enter character to check:")
     freq=0
     for i in string:
         if(char==i):
             freq+=1
     if(freq==0):
-        print("The given alphabet appears 0 times")
+        print("The given character appears 0 times")
     else:
         print(char,"appears",freq,"times\n")
 
 #Function to check palindrome
 def palindrome():
-    str1=input("Enter string to check for palindrome:")
+    str1=input("Enter string:")
     str2=str1[::-1]
     if(str1==str2):
         print(str1,"is a palindrome\n")
@@ -84,27 +59,38 @@ def palindrome():
 #Function to check first appearance
 def first_appearance():
     index=0
-    string=input("Enter string to check for first occurence:")
+    string=input("Enter string:")
     substring=input("Enter substring:")
     for i in string:
         index+=1
         if(i==substring):
             print(substring,"first found at position",index,"\n")
             break;
-        
+            
+#Function to check for occurences of each letter
+def occurrences():
+    string=input("Enter string:")
+    count={}
+    for i in string:
+        if i in count:
+            count[i]+=1
+        else:
+            count[i]=1
+    print(count)
+
 while True:
     print("*******************Menu*******************")
-    choice=int(input(("1.Display longest string\n2.Check the length of longest string\n3.Check frequency of occurence\n4.Check for palindrome\n5.Check for first appearance of substring\n6.Exit\nEnter your choice(1/2/3/4/5/6):")))
+    choice=int(input(("1.Display longest string\n2.Check frequency of occurence\n3.Check for palindrome\n4.Check for first appearance of substring\n5.Check for occurrence of each word\n6.Exit\nEnter your choice(1/2/3/4/5/6):")))
     if choice==1:
         longest()
     elif choice==2:
-        length()
-    elif choice==3:
         frequency()
-    elif choice==4:
+    elif choice==3:
         palindrome()
-    elif choice==5:
+    elif choice==4:
         first_appearance()
+    elif choice==5:
+        occurrences()
     elif choice==6:
         print("Thank you for using this program!")
         break
